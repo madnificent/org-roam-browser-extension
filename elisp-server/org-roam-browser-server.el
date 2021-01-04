bd8b09a80318c49090772ccd89b1197a11739015
Aad Versteden <madnificent@gmail.com>
2021-01-04 17:35:04 +0100

    Add link comments so we can detangle

--- elisp-server/org-roam-browser-server.el
+++ elisp-server/org-roam-browser-server.el
@@ -1,3 +1,4 @@
+;; [[file:../README.org::*The elisp server][The elisp server:1]]
 ;;; org-roam-browser-server -- A package providing information to the browser on what you have stored in org-roam.
 
 ;;; Commentary:
@@ -5,28 +6,34 @@
 ;;; More information at https://github.com/madnificent/org-roam-browser-server.git
 
 ;;; Code:
+;; The elisp server:1 ends here
 
+;; [[file:../README.org::*The elisp server][The elisp server:2]]
 (ws-start
  'org-roam-server-handler
  10001)
+;; The elisp server:2 ends here
 
+;; [[file:../README.org::*The elisp server][The elisp server:3]]
 (defun org-roam-server-handler (request)
   (with-slots (process headers) request
     (ws-response-header process 200 '("Content-type" . "application/json") '("Access-Control-Allow-Origin" . "*"))
     (process-send-string
      process
-     (let ((url (cdr (assoc "url" headers))))
-       (let ((page-exists
-              (org-roam-db-query
-               [:select file :from refs
-                        :where (= ref $v1)]
-               (vector url)))
-             (page-referenced
-              (org-roam-db-query [:select source :from links :where (= links:dest $s1)]
-                                 (vector url))))
-         (concat
-          "{\"pageExists\": " (if page-exists "true" "false") ",\n"
-          " \"linkExists\": " (if page-referenced "true" "false") "}"))))))
+     (let ((page-exists
+            (org-roam-db-query
+             [:select file :from refs
+                      :where (= ref $v1)]
+             (vector (cdr (assoc "url" headers)))))
+           (page-referenced
+            (org-roam-db-query [:select source :from links :where (= links:dest $s1)]
+                               (cdr (assoc "url" headers)))))
+      (concat
+       "{\"pageExists\": " (if page-exists "true" "false") ",\n"
+       " \"linkExists\": " (if page-referenced "true" "false") "}")))))
+;; The elisp server:3 ends here
 
+;; [[file:../README.org::*The elisp server][The elisp server:4]]
 (provide 'org-roam-browser-server)
 ;;; org-roam-browser-server.el ends here
+;; The elisp server:4 ends here
